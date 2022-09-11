b19012e9f9d370db07fb2a5ec77af96a081acd68
yuity0725 <cherryblossoms0725@gmail.com>
2022-09-12 02:50:50 +0900

    front for demo

--- interface/src/App.js
+++ interface/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React, { useEffect, useRef, useState, } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Row, Table } from 'react-bootstrap'
 
 function App() {
 
@@ -13,21 +13,31 @@ function App() {
   const [recognitionStatus, setRecognitionStatus] = useState("認識開始");
   const [visible, setVisible] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [voice, setVoice] = useState("");
   const [log, setLog] = useState([]);
+  const [recommends, setRecommends] = useState([]);
+  const [showRecommends, setShowRecommends] = useState(true);
 
   const refFirstRef = useRef(true);
+  const recommendsRef = useRef < HTMLDivElement > (false);
 
   // stateをセット
-  const handleModifier = e => setModifier(e.target.value);
-  const handleVisible = e => {
-    speechText("こんなドレスはどうですか？")
+  const handleVisible = () => {
+    setVoice("こんなドレスはどうですか？");
     setVisible(true);
   };
   const addFavoriteDress = (newFavoriteDress) => {
     setFavoriteDress([newFavoriteDress, ...favoriteDresses])
   }
+  const addLog = (record) => {
+    console.log(JSON.stringify(record))
+    setLog(log => {
+      const newLog = [...log, JSON.stringify(record)];
+      setLog(newLog);
+      return newLog;
+    })
+  }
 
-  // 読み込み時
   useEffect(() => {
     getImage();
   }, [modifier])
@@ -57,14 +67,19 @@ function App() {
       .then((response) => {
         setImage(response.data)
         console.log(response.data)
+        addLog(response.data);
       })
   }
 
+  useEffect(() => {
+    speechText()
+  }, [voice])
+
   // 読み上げ
-  const speechText = (text) => {
-    console.log(text);
+  const speechText = () => {
+    console.log(voice);
     const speech = new SpeechSynthesisUtterance();
-    speech.text = text;
+    speech.text = voice;
     speech.lang = 'ja-JP';
     window.speechSynthesis.speak(speech)
   }
@@ -76,26 +91,25 @@ function App() {
   recognizer.lang = 'ja-JP';
   // 認識
   recognizer.onresult = (e) => {
+    setRecognitionStatus("読み込み中");
     const results = e.results[0][0].transcript;
     console.log(results);
+    addLog(results);
     setModifier(results);
-    console.log('stop')
     recognizer.stop();
     isListening = false;
-    setRecognitionStatus("認識開始");
+    if (results == modifier) submitText();
   }
   let isListening = false;
   const recognize = (e) => {
     if (!isListening) startRecognizer();
-    console.log(isListening)
   }
 
   const startRecognizer = () => {
     if (isListening) return;
-    console.log('start')
     recognizer.start();
     isListening = true;
-    setRecognitionStatus("認識終了");
+    setRecognitionStatus("認識中");
   }
 
   // 通信
@@ -114,32 +128,65 @@ function App() {
           "modifier": modifierEng,
           "image_path": image
         }
-        console.log(postData)
+        console.log(postData);
+        addLog(postData);
         const postUrl = "http://127.0.0.1:49876/dress"
         const result = axios.post(postUrl, postData)
           .then((responseImage) => {
             console.log(responseImage.data);
+            addLog(responseImage.data)
             if (responseImage.data["new_turn"]) addFavoriteDress(image);
             setImage(responseImage.data["new_image"]);
             setLoading(false);
-            speechText("こちらはどうですか？")
+            setVoice("こちらはどうですか？");
+            if (voice == "こちらはどうですか？") speechText();
+            setRecognitionStatus("認識開始")
           })
           .catch((errorImage) => {
             console.log(errorImage);
+            addLog(errorImage)
           })
       }).catch((errorTrans) => {
         console.log("Could not reach the API: ");
         console.log(errorTrans.message);
+        addLog(errorTrans.message)
       })
 
     setLoading(true);
   }
 
+  const screenHeight = window.innerHeight;
+
+  // 好み推定
+  const getRecommendation = async () => {
+    console.log(recommends)
+    const url = 'http://127.0.0.1:49876/favorite';
+    await axios.get(url)
+      .then((responseRecommend) => {
+        console.log(responseRecommend.data['estimation']);
+        setRecommends(responseRecommend.data['estimation']);
+      })
+  }
+
+  useEffect(() => {
+    const handleClickToCloseRecommends = (event: any) => {
+      const element = recommendsRef.current;
+      console.log(showRecommends)
+      if (element?.contains(event.target)) return;
+      setRecommends([]);
+    };
+
+    window.addEventListener("click", handleClickToCloseRecommends);
+    return () => {
+      window.removeEventListener("click", handleClickToCloseRecommends);
+    };
+  }, [showRecommends, recommendsRef]);
+
   return (
     <Container>
       <Row>
-        <Col md={10}>
-          <div id='start' style={{ visibility: !visible ? "visible" : "hidden" }} onClick={handleVisible}>
+        <Col md={8}>
+          <div id='start' style={{ display: !visible ? "block" : "none" }} onClick={handleVisible}>
             <button>開始</button>
           </div>
           <div className="loader" style={{ visibility: loading ? "visible" : "hidden" }}>Loading...</div>
@@ -147,7 +194,7 @@ function App() {
             <Container>
               <Row>
                 <Col className='questionArea'>
-                  <p>こんなドレスはどうですか？</p>
+                  <p>{voice}</p>
                 </Col>
               </Row>
               <Row>
@@ -156,9 +203,11 @@ function App() {
                 </Col>
                 <Col md={{ span: 2, offset: 8 }} className='modifierArea'>
                   <button onClick={recognize}>{recognitionStatus}</button>
-                  <input type="text" placeholder="Enter modifier" onChange={handleModifier} />
-                  <button onClick={submitText}>送信</button>
                 </Col>
+              </Row>
+              <Row>
+                <button onClick={getRecommendation}>おすすめを見る</button>
+                <Recommend recommends={recommends} />
               </Row>
               <Row className='favoriteArea'>
                 <Col md={{ span: 8, offset: 2 }}>
@@ -170,11 +219,40 @@ function App() {
             </Container>
           </div>
         </Col>
-        <Col md={2}>
+        <Col md={4} style={{ height: screenHeight }} className='logArea'>
+          <Log log={log} visible={visible} />
         </Col>
       </Row>
     </Container>
   );
+}
+
+const Log = (props) => {
+  return (
+    <Table style={{ visibility: props.visible ? "visible" : "hidden" }}>
+      <tbody>
+        {props.log.map(record =>
+          <tr>
+            <td>
+              {record}
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </Table>
+  )
+}
+
+const Recommend = (props) => {
+  return (
+    <div id='recommendation'>
+      {
+        props.recommends.map(recommend =>
+          <img src={`${process.env.PUBLIC_URL}/images/${recommend}.png`} alt="recommendation" />
+        )
+      }
+    </div>
+  )
 }
 
 export default App;
